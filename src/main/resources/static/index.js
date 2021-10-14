0b1841e47db91f53d2970606696de8205e41678e
onlyak <ameykale603@gmail.com>
2021-10-14 13:50:03 +0100

    Basic Rectangle with random movement

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-    $("#div").html(123)
-
-    alert(123)
-    $("#test").click(function () {
-        alert(321);
-    });
+    moveDiv();
 });
 
+function makeNewPosition(){
 
+    var h = $(window).height() - 300;
+    var w = $(window).width() - 400;
+
+    var nh = Math.floor(Math.random() * h * 0.6);
+    var nw = Math.floor(Math.random() * w * 0.7);
+
+    return [nh,nw];
+}
+
+function moveDiv(){
+    var newq = makeNewPosition();
+    $('.basicRectangle').animate({ top: newq[0], left: newq[1]},1000, function(){
+      moveDiv();
+    });
+
+};
