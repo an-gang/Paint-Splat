136989e336fa63e01df9a6cd1c853b1fe8dbecb9
TIANYI HU <ceiling121388@aliyun.com>
2021-10-22 23:21:26 +0100

    working10222321

--- src/main/java/com/wangdefa/paint_splat/Other/ID_generator.java
+++ src/main/java/com/wangdefa/paint_splat/Other/ID_generator.java
@@ -1,16 +1,19 @@
 package com.wangdefa.paint_splat.Other;
 
 import java.util.ArrayList;
-
+/**
+ * 根据ID生成六位随机码
+ * @param id ID
+ * @return 随机码
+ */
 public class ID_generator {
+    // 生成六位数id转string
     public static String generateID(){
+        //直接截取时间戳毫秒后六位
+        int id = System.currentTimeMillis();
+        String result = Integer.toString(id).substring(s.length()-6,s.length());//或者事
 
-
-
-        return "004582";
+        return result;
     }
-
-
-
 
 }
