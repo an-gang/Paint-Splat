b51e9423fea6d7acffcc151a4bff141b6485d32b
Gang An <15666671996@163.com>
2021-10-22 23:46:40 +0100

    working10222346

--- src/main/java/com/wangdefa/paint_splat/Other/ID_generator.java
+++ src/main/java/com/wangdefa/paint_splat/Other/ID_generator.java
@@ -1,12 +1,16 @@
 package com.wangdefa.paint_splat.Other;
 
+import java.util.Random;
 
 public class ID_generator {
     // 生成六位数id转string
-    public static String generateID(){
-        int id = (int)((Math.random()*9+1)*100000);
-        String str = Integer.toString(id);
-        return str;
+    public static String generateID() {
+        Random random = new Random();
+        String id = Integer.toString(random.nextInt(1000000));
+        while (id.length() < 6) {
+            id = "0" + id;
+        }
+        return id;
     }
 
 }
