6834b539d566b7dfc54aeca6716ee8bdeabff728
TIANYI HU <ceiling121388@aliyun.com>
2021-10-22 22:50:56 +0100

    working10222249

--- src/main/java/com/wangdefa/paint_splat/service/Game_Service.java
+++ src/main/java/com/wangdefa/paint_splat/service/Game_Service.java
@@ -6,6 +6,11 @@ import com.wangdefa.paint_splat.entity.Game;
 import org.springframework.stereotype.Service;
 
 import java.util.Timer;
+import java.text.DateFormat;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
 
 @Service
 public class Game_Service implements Game_Service_Interface {
@@ -28,6 +33,27 @@ public class Game_Service implements Game_Service_Interface {
         GameTimer gameTimer = new GameTimer();
         gameTimer.setGame(game);
         timer.schedule(gameTimer, 0, 1000);
+    }
+    public void run(Game game){
+        DateFormat dateFormat = new SimpleDateFormat();
+
+        String paramTime = new SimpleDataFormat().format(game.getStartTime().getTime()); // 游戏开始时间
+
+        String systemTime = new SimpleDateFormat().format(new Date().getTime());// 获取系统时间
+
+        long difference = 0; // 差值
+
+        try {
+            Date systemDate = dateFormat.parse(systemTime);
+
+            Date paramDate = dateFormat.parse(paramTime);
+
+            difference = systemDate.getTime() - paramDate.getTime();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        game.setTimeSpan(difference); // 返回差值时间
     }
 
 
