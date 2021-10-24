ede0338a7e5007fcf6a566cab5d9451f257fd2b7
Gang An <15666671996@163.com>
2021-10-24 17:44:33 +0100

    working10241744

--- src/main/resources/static/paint_splat.js
+++ src/main/resources/static/paint_splat.js
@@ -2,20 +2,18 @@ $(document).ready(function () {
     var room = $("#roomNumber");
     var player = $("#scoreBoard");
     var scores = $("#playergitScore");
-    var playerNum=0;
-    var roomid="";
+    var playerNum = 0;
     var playerscore;
-    $.post("/checkRoomId",{},function (roomId) {
-        roomid = roomId;
-        room.text(roomid);
-    })
-    $.post("/getGame",{},function (data) {
+    $.post("/getGame", {}, function (data) {
         console.log(data);
         console.log(data["scores"]);
         playerscore = data["scores"];
     });
-    setInterval(function (){$.post("/countPlayer",{},function (playerNum) {
-            playerNum = playerNum;
-        });}
-        ,5000);
+    setInterval(function () {
+            $.post("/countPlayer", {}, function (playerNum) {
+                playerNum = playerNum;
+                console.log(playerNum);
+            });
+        }
+        , 5000);
 });
