71062c47c3f1998290d59baabcba9616bb7b588c
zhuyahong <981587061@qq.com>
2021-10-24 16:33:12 +0100

    working10241633

--- src/main/resources/static/paint_splat.js
+++ src/main/resources/static/paint_splat.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-    var roomid = $.post("/checkRoomId",{},function (roomId) {
+    var room = $("#roomNumber");
+    var player = $("#scoreBoard");
+    var scores = $("#playerScore");
+    $.post("/checkRoomId",{},function (roomId) {
+        var roomid = roomId;
+        room.text(roomid);
+        var playerid = 0;
+        var playerscore;
         $.post("/getGame",{roomId: roomId},function (data) {
             console.log(data);
-        })
-    var room = $("#roomNumber");
-    room.text(roomid);
+            console.log(data["scores"]);
+            playerid = data["players"][0];
+            playerscore = data["scores"];
+            player.text(playerid);
+            scores.text(playerscore);
+        });
     })
 });
